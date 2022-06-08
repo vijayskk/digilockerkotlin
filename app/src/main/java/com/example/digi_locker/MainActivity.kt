72d0902551cc7085765544fa895bcb6c55ae4417
vijayskk <vijayskk007@gmail.com>
2022-06-08 06:51:06 +0530

    Basic app ok

--- app/src/main/java/com/example/digi_locker/MainActivity.kt
+++ app/src/main/java/com/example/digi_locker/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.digi_locker
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
     }
     fun onButton(view:View){
         var tag : Int = (view.tag as String).toInt()
--- app/src/main/java/com/example/digi_locker/ViewActivity.kt
+++ app/src/main/java/com/example/digi_locker/ViewActivity.kt
@@ -2,15 +2,18 @@ package com.example.digi_locker
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import java.io.File
-import java.lang.Exception
+import java.io.FileOutputStream
+
 
 class ViewActivity : AppCompatActivity() {
     val REQ_CODE = 1
@@ -22,6 +25,14 @@ class ViewActivity : AppCompatActivity() {
         setContentView(R.layout.activity_view)
         var title = findViewById<TextView>(R.id.title)
         title.text = AppConstants().sections[curr!!]
+
+        val file: File = applicationContext.getFileStreamPath("$curr.png")
+        if(file.exists()){
+            val imageFullPath: String = file.getAbsolutePath()
+            findViewById<ImageView>(R.id.image1).setImageURI(Uri.parse(imageFullPath))
+        }else{
+            findViewById<ImageView>(R.id.image1).setImageResource(R.drawable.ic_launcher_background)
+        }
 
 
     }
@@ -41,12 +52,27 @@ class ViewActivity : AppCompatActivity() {
 
         if (requestCode == REQ_CODE && resultCode == RESULT_OK) {
             selectedFile = data?.data
-            saveImageAndReload()
+            if(selectedFile != null){
+                saveImageAndReload(selectedFile!!)
+            }
+
         }
     }
-    fun saveImageAndReload(){
-        findViewById<ImageView>(R.id.image1).setImageURI(selectedFile)
-        findViewById<TextView>(R.id.title).text = selectedFile.toString()
-
+    fun saveImageAndReload(uri: Uri){
+        findViewById<ImageView>(R.id.image1).setImageURI(uri)
+        val curr = AppConstants().getCurrentScreen()
+        val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
+        saveImage(this,bitmap, "$curr.png")
+    }
+    fun saveImage(context: Context, b: Bitmap, imageName: String?) {
+        val foStream: FileOutputStream
+        try {
+            foStream = context.openFileOutput(imageName, MODE_PRIVATE)
+            b.compress(Bitmap.CompressFormat.PNG, 100, foStream)
+            foStream.close()
+        } catch (e: Exception) {
+            Toast.makeText(this,"Exception 2, Something went wrong!",Toast.LENGTH_SHORT).show()
+            e.printStackTrace()
+        }
     }
 }
